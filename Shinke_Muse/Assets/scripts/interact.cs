f9c8b9ef880fe5fed78a8d3d997ea1008fcc4760
Marinusduif <34818@ma-web.nl>
2024-06-06 15:26:30 +0200

    interacteringezet

---
+++ Shinke_Muse/Assets/scripts/interact.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    RaycastHit hit;
+    Outline outline;
+    [SerializeField] private float interactDistance;
+    public LayerMask interactable;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, interactDistance, interactable))
+        {
+            outline = hit.transform.gameObject.GetComponent<Outline>();
+            outline.enabled = true;
+
+            if (hit.transform.CompareTag("destroyable"))
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+        else if (outline != null)
+        {
+            outline.enabled = false;
+            outline = null;
+            return;
+        }
+    }
+}
+
+
